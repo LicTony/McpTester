4ee8f658de34d48bde3f4e58c9a2ace6043440de
LicTony <antoniosoft@hotmail.com>
2026-03-03 10:47:02 -0300

    Version Inicial

---
+++ McpTester/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace McpTester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
